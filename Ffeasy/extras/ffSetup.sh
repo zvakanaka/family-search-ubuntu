64cb36472b41f0aeb43f51e59c8875c0e4df6073
zvakanaka <quintonish@gmail.com>
2014-08-28 11:23:16 -0700

    Delete ffSetup.sh

--- Ffeasy/extras/ffSetup.sh
+++
@@ -1,4 +0,0 @@
-sudo cp ffall.sh /usr/bin/ffall
-sudo cp ../ffeasy.sh /usr/bin/ffeasy
-sudo cp ffmpeg.xpm /usr/share/pixmaps/ffmpeg.xpm
-sudo cp ffeasy.desktop /usr/share/applications/ffeasy.desktop
